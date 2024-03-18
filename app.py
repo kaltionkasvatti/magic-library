6d3a40ab3f27a1d4d8334732bc266dee56cf619c
Hánno Länsman <halansman@gmail.com>
2024-03-18 15:20:47 +0200

    Removed venv from repo

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2:///hannolan"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    result = db.session.execute(text("SELECT name FROM cards"))
+    cards = result.fetchall()
+    return render_template("index.html", count=len(cards), cards=cards)
