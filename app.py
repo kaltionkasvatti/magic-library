b88cbebc54011c8ffbdec6e1706962ef529b0dad
Hánno Länsman <halansman@gmail.com>
2024-03-22 11:03:11 +0200

    Usernames and passwords added to db, next is displaying data per user

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sqlalchemy.sql import text
 from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
@@ -21,6 +22,15 @@ def index():
 def newcard():
     return render_template("newcard.html")
 
+@app.route("/signin", methods=["POST"])
+def signin():
+    hash_value = generate_password_hash(request.form["password"])
+    username = request.form["username"]
+    sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+    db.session.execute(text(sql), {"username":username, "password":hash_value})
+    db.session.commit()
+    return redirect("/")
+
 @app.route("/cardsend", methods=["POST"])
 def cardsend():
     cardname = request.form["cardname"]
@@ -33,11 +43,25 @@ def cardsend():
 def login():
     username = request.form["username"]
     password = request.form["password"]
-    # TODO: check username and password
-    session["username"] = username
-    return redirect("/")
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(text(sql), {"username":username})
+    user = result.fetchone()    
+    if not user:
+        return redirect("/ohno")
+    else:
+        hash_value = user[1]
+        if check_password_hash(hash_value, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return redirect("/ohno")
+    
 
 @app.route("/logout")
 def logout():
     del session["username"]
     return redirect("/")
+
+@app.route("/ohno")
+def ohno():
+    return render_template("ohno.html")
