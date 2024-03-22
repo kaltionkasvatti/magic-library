2930ad6fde218bf7b96b8068867019695bf2b303
Hánno Länsman <halansman@gmail.com>
2024-03-22 10:09:07 +0200

    Logging in now works, next is passwords and users to database

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import render_template, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
