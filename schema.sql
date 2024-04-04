35199fe84681b692ff006f36fbbf446804d17ba5
Hánno Länsman <halansman@gmail.com>
2024-04-04 14:54:24 +0300

    Added schema for local install

---
+++ schema.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS cards CASCADE;
+DROP TABLE IF EXISTS libraries CASCADE;
+DROP TABLE IF EXISTS cardlib CASCADE;
+DROP TABLE IF EXISTS backside CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE cards (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    twofaced BOOLEAN,
+    colour TEXT,
+    cmc TEXT,
+    rarity TEXT,
+    power TEXT,
+    toughness TEXT,
+    userid INTEGER,
+    photo INTEGER
+);
+
+CREATE TABLE libraries (
+    id SERIAL PRIMARY KEY,
+    userid INTEGER,
+    name TEXT
+);
+
+CREATE TABLE cardlib (
+    card INTEGER,
+    library INTEGER,
+    visible BOOLEAN
+);
+
+CREATE TABLE backside (
+    id SERIAL PRIMARY KEY,
+    frontid INTEGER,
+    name TEXT,
+    cmc TEXT,
+    power TEXT,
+    toughness TEXT
+);
