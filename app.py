a16306d65fd8d42eb9b40fbb3a5ab7fcd72e2fa7
Hánno Länsman <halansman@gmail.com>
2024-04-10 15:34:29 +0300

    Adding new cards now works

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def index():
         folders = finds.fetchall()
         result = db.session.execute(text("""
             SELECT C.name, C.id FROM cards C, users U , cardlib D 
-            WHERE C.userid=U.id  AND C.id=D.card AND D.visible=True AND U.username=:username"""), {"username":session["username"]})
+            WHERE C.userid=U.id  AND C.id=D.card AND D.visible=True AND D.library=0 AND U.username=:username"""), {"username":session["username"]})
         cards = result.fetchall()
         return render_template("index.html", count=len(cards), cards=cards, folders=folders, number=len(folders))
     else:
@@ -43,8 +43,46 @@ def signin():
 @app.route("/cardsend", methods=["POST"])
 def cardsend():
     cardname = request.form["cardname"]
-    sql= "INSERT INTO cards (name) VALUES (:cardname)"
-    db.session.execute(text(sql), {"cardname":cardname})
+    try: two_faced = request.form["twofaced"]
+    except: two_faced = False
+    try: colour1 = request.form["colour1"]
+    except: colour1 = ""
+    try: colour2 = request.form["colour2"]
+    except: colour2 = ""
+    try: colour3 = request.form["colour3"]
+    except: colour3 = ""
+    try: colour4 = request.form["colour4"]
+    except: colour4 = ""
+    try: colour5 = request.form["colour5"]
+    except: colour5 = ""
+    colours = colour1 + colour2 + colour3 + colour4 + colour5
+    cmc = request.form["cmc"]
+    rarity = request.form["rarity"]
+    try: power = request.form["power"]
+    except: power = None
+    try: toughness = request.form["toughness"]
+    except: toughness = None
+    inlibs = []
+    libs = request.form["libs"]
+    for lib in libs:
+        try: inlibs.append((lib[1], request.form[lib[0]]))
+        except: pass
+    user = db.session.execute(text("SELECT id FROM users WHERE username=:username"), {"username":session["username"]}).fetchone()[0]
+    sql= """INSERT INTO cards (name, twofaced, colour, cmc, rarity, power, toughness, userid) 
+            VALUES (:cardname, :twofaced, :colours, :cmc, :rarity, :power, :toughness, :user)"""
+    db.session.execute(text(sql), {"cardname":cardname, "twofaced":two_faced, "colours":colours, "cmc":cmc, "rarity":rarity, "power":power, "toughness":toughness, "user":user})
+    db.session.commit()
+    userid = db.session.execute(text("""
+                            SELECT id FROM cards WHERE name=:cardname AND rarity=:rarity AND colour=:colours ORDER BY id DESC LIMIT 1"""
+                            ), {"cardname":cardname, "rarity":rarity, "colours":colours}).fetchone()[0]
+    print(userid)
+    if len(inlibs) != 0:
+        for i in inlibs:
+            sql = "INSERT INTO cardlib (card, library, visible) VALUES (:userid, :library, True)"
+            db.session.execute(text(sql), {"userid":userid, "library":i[0]})
+    else:
+        sql = "INSERT INTO cardlib (card, library, visible) VALUES (:userid, 0, True)"
+        db.session.execute(text(sql), {"userid":userid})
     db.session.commit()
     return redirect("/")
 
