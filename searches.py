030f9ca306a58af442da07c7b902b9d46f20bcd4
Hánno Länsman <halansman@gmail.com>
2024-04-18 14:04:58 +0300

    Added a general search function, yet to test it

--- searches.py
+++ searches.py
@@ -2,7 +2,20 @@ from db import db
 from flask import session
 from sqlalchemy.sql import text
 
-def seekname(name, user, folder):
+def seekcard(
+            user: str, 
+            folder: int,
+            name = None,
+            twofaced = None,
+            colours = None,
+            rarity = None,
+            power = None, #str
+            toughness = None #str
+            ):
+    
+    variables = {}
+    variables["user"] = user
+    variables["folder"] = folder
     frontseek = """SELECT 
             C.id, 
             C.name, 
@@ -14,29 +27,53 @@ def seekname(name, user, folder):
             FROM cards C, cardlib D, users U
             WHERE C.id=D.card 
             AND C.userid=U.id
-            AND C.name LIKE :name
             AND U.username = :user
             AND D.library = :folder
             """
     
+    if name is not None:
+        frontseek = frontseek + " AND C.name LIKE :name"
+        variables["name"] = '%' + name + '%'
+
+    if twofaced is not None:
+        frontseek = frontseek + " AND C.twofaced = :twofaced"
+        variables["twofaced"] = twofaced
+
+    if colours is not None:
+        for i in range(0,len(colours)):
+            frontseek = frontseek + " AND C.colour LIKE :colour" + str(i)
+            variables["colour" + str(i)] = '%' + colours[i] + '%'
+
+    if rarity is not None:
+        frontseek = frontseek + " AND C.rarity = :rarity"
+        variables["rarity"] = rarity
+
+    if power is not None and len(power) > 1:
+        frontseek = frontseek + " AND C.power " + power[0] + " " + str(int(power[1:]))
+
+    if toughness is not None and len(toughness) > 1:
+        frontseek = frontseek + " AND C.toughness " + toughness[0] + " " + str(int(toughness[1:]))
+    
+    front = db.session.execute(text(frontseek), variables).fetchall()
+
+    
     backseek = """SELECT
-            B.id,
-            B.name,
-            B.cmc,
-            B.power,
-            B.toughness
-            FROM cards C, cardlib D, backside B, users U
-            WHERE C.id = B.frontid
-            AND B.frontid = D.card
-            AND C.userid = U.id
-            AND B.name LIKE :name
-            AND U.username = :user
-            AND D.library = :folder
+            id,
+            name,
+            cmc,
+            power,
+            toughness
+            FROM backside
+            WHERE id = 0
             """
-    front = db.session.execute(text(frontseek), {"name":name, "user":user, "folder":folder}).fetchall()
-    back = db.session.execute(text(backseek), {"name":name, "user":user, "folder":folder}).fetchall()
+    
+    for card in front:
+        backseek = backseek + " OR frontid = " + str(card[0])
+
+    back = db.session.execute(text(backseek)).fetchall()
     return (front, back)
 
 def seekuser(username):
     sql = """SELECT id FROM users WHERE username=:username"""
     return db.session.execute(text(sql), {"username":username}).fetchall()
+
